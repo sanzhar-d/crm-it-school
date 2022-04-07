e00ff43d7f50990a07edb163f5f17817feb3d434
Sanzhar <sanzararzymatov97511@gmail.com>
2022-04-07 17:40:44 +0600

    okHttp added

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,7 +8,11 @@ import com.example.models.Course;
 import com.example.models.CourseFormat;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import okhttp3.*;
 
+
+import java.io.IOException;
 import java.util.List;
 
 public class Main {
@@ -16,12 +20,12 @@ public class Main {
         System.out.println("test");
 
       CourseFormatDaoImpl courseFormatDao = new CourseFormatDaoImpl();
-       CourseFormat courseFormat = new CourseFormat();
-       courseFormat.setFormat("Regular");
-       courseFormat.setOnline(true);
-       courseFormat.setLessonsPerWeek(3);
-       courseFormat.setLessonDuration(90);
-       courseFormat.setDurationWeeks(28);
+//       CourseFormat courseFormat = new CourseFormat();
+//       courseFormat.setFormat("Regular");
+//       courseFormat.setOnline(true);
+//       courseFormat.setLessonsPerWeek(3);
+//       courseFormat.setLessonDuration(90);
+//       courseFormat.setDurationWeeks(28);
 //       //сохранение объекта CourseFormat
 //     CourseFormat courseFormatSaved = courseFormatDao.save(courseFormat);
 //       //получение объекта по id
@@ -42,29 +46,54 @@ public class Main {
 //        Branch branch1 = new Branch(6L, "kjhjh", "766");
 //        branch1.setAddress("fghgfghf");
 
+        CourseDaoImpl courseDao = new CourseDaoImpl();
+
+        Course course = courseDao.findById(1L);
+
 //        ObjectMapper objectMapper = new ObjectMapper();
+//        Gson gson = new Gson();
+
 //        try {
-//            String json = objectMapper.writeValueAsString(fromDB);
+//            String json = objectMapper.writeValueAsString(course);
 //            System.out.println(json);
-//            CourseFormat courseFormat = objectMapper.readValue(json, CourseFormat.class);
+//            Course course1 = objectMapper.readValue(json, Course.class);
+//            System.out.println();
 //
 //        } catch (JsonProcessingException e) {
 //            e.printStackTrace();
 //        }
 
-        CourseDaoImpl courseDao = new CourseDaoImpl();
-        Course course = new Course();
-        course.setName("Java");
-        course.setPrice(8000);
-        course.setCourseFormat(fromDB);
-        Course saved = courseDao.save(course);
+//        String json  = gson.toJson(course);
+//        System.out.println(json);
+//        Course course1 = gson.fromJson(json, Course.class);
 
-        CourseFormat format = courseFormatDao.findById(1L);
-        Course python = new Course();
-        python.setName("Python");
-        python.setPrice(9000);
-        python.setCourseFormat(format);
-        Course savedPython = courseDao.save(python);
+
+//        Course course = new Course();
+//        course.setName("Java");
+//        course.setPrice(8000);
+//        course.setCourseFormat(fromDB);
+//        Course saved = courseDao.save(course);
+//
+//       CourseFormat format = courseFormatDao.findById(1L);
+//        Course python = new Course();
+//        python.setName("Python");
+//        python.setPrice(9000);
+//        python.setCourseFormat(format);
+//        Course savedPython = courseDao.save(python);
+
+        String url = "http://localhost:8080/v1/home/page";
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
